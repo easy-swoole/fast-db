827f855551541e80eada78f4a54169d06a73a390
如果的如果 <admin@fosuss.com>
2022-11-30 22:24:12 +0800

    1

--- src/FastDb.php
+++ src/FastDb.php
@@ -2,7 +2,44 @@
 
 namespace EasySwoole\FastDb;
 
+use EasySwoole\Mysqli\QueryBuilder;
+
 class FastDb
 {
+    protected QueryBuilder|null $queryBuilder = null;
 
+    function __construct()
+    {
+        $this->queryBuilder = new QueryBuilder();
+    }
+
+    function getOne(string $targetEntity):?Entity
+    {
+        return null;
+    }
+
+    function all(string $targetEntity)
+    {
+
+    }
+
+    function save(Entity $entity)
+    {
+
+    }
+
+    function update(Entity $entity)
+    {
+
+    }
+
+    function delete(Entity $entity)
+    {
+
+    }
+
+    function chunk(callable $func,string $targetEntity,$chunkSize = 10)
+    {
+
+    }
 }
