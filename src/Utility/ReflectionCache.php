40ac93961084f5a3dec23b29019ada299ab67e42
如果的如果 <admin@fosuss.com>
2022-12-13 00:10:30 +0800

    1

--- src/Utility/ReflectionCache.php
+++ src/Utility/ReflectionCache.php
@@ -3,8 +3,24 @@
 namespace EasySwoole\FastDb\Utility;
 
 use EasySwoole\Component\Singleton;
+use EasySwoole\FastDb\Entity;
+use EasySwoole\FastDb\Exception\RuntimeError;
 
 class ReflectionCache
 {
     use Singleton;
+
+    private array $cache = [];
+
+    function entityReflection(string $entityClass)
+    {
+        $key = md5($entityClass);
+        if(isset($this->cache[$key])){
+            return $this->cache[$key];
+        }
+        $ref = new \ReflectionClass($entityClass);
+        if(!$ref->isSubclassOf(Entity::class)){
+            throw new RuntimeError("relate targetEntity class {$entityClass} not a sub class of ".Entity::class);
+        }
+    }
 }
