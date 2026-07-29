c4c9580c974a2d15ee42c49db3ff79552f54bc83
如果的如果 <admin@fosuss.com>
2026-07-29 23:23:10 +0800

    分割ListResult实现

---
+++ src/Beans/ArrayList.php
@@ -0,0 +1,108 @@
+<?php
+
+namespace EasySwoole\FastDb\Beans;
+
+use EasySwoole\FastDb\AbstractInterface\AbstractEntity;
+
+class ArrayList  implements \Iterator , \JsonSerializable, \Countable , \ArrayAccess
+{
+    private array $data = [];
+
+    private int $iteratorKey = 0;
+
+    function __construct(array $data)
+    {
+        $this->data = $data;
+    }
+
+    public function current(): mixed
+    {
+        return $this->data[$this->iteratorKey];
+    }
+
+    public function next(): void
+    {
+        $this->iteratorKey++;
+    }
+
+    public function key(): mixed
+    {
+        return $this->iteratorKey;
+    }
+
+    public function valid(): bool
+    {
+        return isset($this->data[$this->iteratorKey]);
+    }
+
+    public function rewind(): void
+    {
+        $this->iteratorKey = 0;
+    }
+
+    function list():array
+    {
+        return $this->data;
+    }
+
+    function first():array|AbstractEntity|null
+    {
+        if(isset($this->data[0])){
+            return $this->data[0];
+        }
+        return null;
+    }
+
+    public function jsonSerialize(): array
+    {
+        return $this->data;
+    }
+
+    public function count(): int
+    {
+        return count($this->data);
+    }
+
+    public function offsetExists(mixed $offset): bool
+    {
+        return isset($this->data[$offset]);
+    }
+
+    public function offsetGet(mixed $offset): mixed
+    {
+        if(isset($this->data[$offset])){
+            return $this->data[$offset];
+        }
+        return null;
+    }
+
+    public function offsetSet(mixed $offset, mixed $value): void
+    {
+        $this->data[$offset] = $value;
+    }
+
+    public function offsetUnset(mixed $offset): void
+    {
+        unset($this->data[$offset]);
+    }
+
+
+    function toArray(): array
+    {
+        return $this->data;
+    }
+
+    function remove(int|AbstractEntity $indexOrEntity): void
+    {
+        if($indexOrEntity instanceof AbstractEntity){
+            foreach ($this->data as $key => $item){
+                if($item === $indexOrEntity){
+                    array_splice($this->data, $key, 1);
+                    break;
+                }
+            }
+        }else{
+            array_splice($this->data, $indexOrEntity, 1);
+        }
+    }
+}
--- src/Beans/ListResult.php
+++ src/Beans/ListResult.php
@@ -4,114 +4,20 @@ namespace EasySwoole\FastDb\Beans;
 
 use EasySwoole\FastDb\AbstractInterface\AbstractEntity;
 
-class ListResult  implements \Iterator , \JsonSerializable, \Countable , \ArrayAccess
+class ListResult extends ArrayList
 {
 
-    private array $data = [];
     private ?int $totalCount = null;
 
     function __construct(array $data,int|null $totalCount = null)
     {
-        $this->data = $data;
+        parent::__construct($data);
         $this->totalCount = $totalCount;
-    }
-
-    private int $iteratorKey = 0;
-
-    public function current(): mixed
-    {
-        return $this->data[$this->iteratorKey];
-    }
-
-    public function next(): void
-    {
-        $this->iteratorKey++;
-    }
-
-    public function key(): mixed
-    {
-        return $this->iteratorKey;
-    }
-
-    public function valid(): bool
-    {
-        return isset($this->data[$this->iteratorKey]);
-    }
-
-    public function rewind(): void
-    {
-        $this->iteratorKey = 0;
-    }
-
-    function list():array
-    {
-        return $this->data;
-    }
-
-    function first():array|AbstractEntity|null
-    {
-        if(isset($this->data[0])){
-            return $this->data[0];
-        }
-        return null;
     }
 
     function totalCount():?int
     {
         return $this->totalCount;
-    }
-
-    public function jsonSerialize(): mixed
-    {
-        return $this->data;
-    }
-
-    public function count(): int
-    {
-        return count($this->data);
-    }
-
-    public function offsetExists(mixed $offset): bool
-    {
-        return isset($this->data[$offset]);
-    }
-
-    public function offsetGet(mixed $offset): mixed
-    {
-        if(isset($this->data[$offset])){
-            return $this->data[$offset];
-        }
-        return null;
-    }
-
-    public function offsetSet(mixed $offset, mixed $value): void
-    {
-        $this->data[$offset] = $value;
-    }
-
-    public function offsetUnset(mixed $offset): void
-    {
-        unset($this->data[$offset]);
-    }
-
-
-    function toArray(): array
-    {
-        return $this->data;
-    }
-
-    function remove(int|AbstractEntity $indexOrEntity): void
-    {
-        if($indexOrEntity instanceof AbstractEntity){
-            foreach ($this->data as $key => $item){
-                if($item === $indexOrEntity){
-                    array_splice($this->data, $key, 1);
-                    break;
-                }
-            }
-        }else{
-            array_splice($this->data, $indexOrEntity, 1);
-        }
     }
 
 }
